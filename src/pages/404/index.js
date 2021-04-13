8ec82299be6c79b7866a8169e081dd6f328c3e35
Joshua Allan <joshua.allan@LC-LTM045.local>
2021-04-13 11:54:15 +0100

    Changed class to className

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 class NotFound extends Component {
   render() {
     return (
-      <div class="row">
-        <div class="col s12 m6 offset-m3">
-          <div class="card">
-            <div class="card-content">
-              <span class="card-title">Oops</span>
+      <div className="row">
+        <div className="col s12 m6 offset-m3">
+          <div className="card">
+            <div className="card-content">
+              <span className="card-title">Oops</span>
               <p>Looks like you've taken a wrong turn!</p>
               <p>You can return to the home page using the link below or check out the other pages in the nav bar :)</p>
             </div>
-            <div class="card-action">
+            <div className="card-action">
             <Link to="/">
                    Return to homepage
              </Link>
