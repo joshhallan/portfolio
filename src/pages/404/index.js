b9b4e9a6e2a58adef2bd62f666520f0a2f189300
Joshua Allan <joshua.allan@LC-LTM045.local>
2021-04-13 11:52:47 +0100

    Styled 404 page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class NotFound extends Component {
   render() {
-    return(
-      <div>Looks like you've taken a wrong turn</div>
+    return (
+      <div class="row">
+        <div class="col s12 m6 offset-m3">
+          <div class="card">
+            <div class="card-content">
+              <span class="card-title">Oops</span>
+              <p>Looks like you've taken a wrong turn!</p>
+              <p>You can return to the home page using the link below or check out the other pages in the nav bar :)</p>
+            </div>
+            <div class="card-action">
+            <Link to="/">
+                   Return to homepage
+             </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
