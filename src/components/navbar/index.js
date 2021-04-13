d33fc7c51ecf0ab71de32262d399e2208baceff2
Joshua Allan <joshua.allan@LC-LTM045.local>
2021-04-13 11:31:35 +0100

    Fixed safari bug

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -86,7 +86,7 @@ class Navbar extends Component {
             </a>
           </li>
           <li>
-            <a href="mailto:joshuaallan@live.co.uk?subject=Hi Josh&body=Talk to me">
+            <a href="mailto:hello@findjosh.dev?subject=Hi Josh&body=Talk to me">
               <i className="fas fa-envelope"></i>Contact
             </a>
           </li>
